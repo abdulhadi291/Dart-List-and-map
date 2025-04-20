1b9911aee734e92ef1ed6bad3d684f7dd8d480e1
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-04-20 14:10:15 +0500

    map

--- list.dart
+++ list.dart
@@ -12,14 +12,32 @@ void main(){
   // print(students.first);
   // print(students.last);
   // print(students.elementAt(4));
-  print(students);
+  // print(students);
 
   
 // students.replaceRange(1,3, ["Asghar" , "Shakoor"]);
 //   print(students);
 // students.sort();
 //   print(students);
-var reverselist = List.of(students.reversed);   // to convert in list
-  print(reverselist);
+// var reverselist = List.of(students.reversed);   // to convert in list
+//   print(reverselist);
+//   students.clear();
+
+Map students1 ={
+  "name" : "hadi",
+  "age" : 20,
+   "gender" :  "male",
+   "city" : "karachi",
+};
+print(students1);
+print(students1.isEmpty);
+
+print(students1.isNotEmpty);
+print(students1.length);
+print(students1.keys);
+print(students1.values);
+print(students1["name"]);
+print(students1["age"]);    
+print(students1["gender"]);
 
 }
