74f8d2834e0cc0c34a867f3a2e1b30d70cd7dd45
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-04-20 13:47:57 +0500

    List

---
+++ list.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  List students = [
+    "hadi",
+    "owais",
+    "ramish",
+    "ali",
+  ];
+  // print(students);
+  // print(students[0]);
+  // print(students.length);
+  // print(students.first);
+  // print(students.last);
+  // print(students.elementAt(4));
+  print(students);
+
+  
+// students.replaceRange(1,3, ["Asghar" , "Shakoor"]);
+//   print(students);
+// students.sort();
+//   print(students);
+var reverselist = List.of(students.reversed);   // to convert in list
+  print(reverselist);
+
+}
