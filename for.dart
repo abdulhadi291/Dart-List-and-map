1c4606cfe42c6eed8e5b806fbe578fee1c521231
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-05-04 12:29:29 +0500

    for loop in list

--- for.dart
+++ for.dart
@@ -1,6 +1,14 @@
 void main(){
    for(int i=3;i<100;i=i+2){
 
-      print("2 x $i = ${2*i} "); 
+      // print("2 x $i = ${2*i} "); 
   }
+
+List<String> names = ["Ali", "Ahmed", "Sara", "Fatima"];
+  for (int i = 0; i < names.length; i++) {
+    print("This is ${names[i]}");
+  }
+  
+  
+
 }
