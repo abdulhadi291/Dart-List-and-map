96c680a6db44e684daecd773b1d15f45e29d81fb
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-05-11 12:27:28 +0500

    Functions

---
+++ functions.dart
@@ -0,0 +1,22 @@
+void main(){
+  print("FUNCTIONS");
+print("=========");
+  printMyDetails(){
+    print("My name is Hadi");
+    print("My age is 22");
+    print("Software Engineer");
+  }
+
+  addTwoNumbers(){
+    int a =93;
+    int b = 7;
+    int sum = a + b;
+    print( "The sum of $a and $b is $sum");
+  }
+  print("This is function 1");
+
+
+  printMyDetails();
+  print("This is  function 2");
+  addTwoNumbers();
+}
