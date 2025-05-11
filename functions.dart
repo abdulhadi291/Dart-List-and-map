0470852d074bcfaccee0c322696461b758745802
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-05-11 13:05:09 +0500

    Functions (int return type)

--- functions.dart
+++ functions.dart
@@ -1,22 +1,36 @@
 void main(){
   print("FUNCTIONS");
 print("=========");
-  printMyDetails(){
-    print("My name is Hadi");
-    print("My age is 22");
-    print("Software Engineer");
+  // printMyDetails(){
+  //   print("My name is Hadi");
+  //   print("My age is 22");
+  //   print("Software Engineer");
+  // }
+
+  // addTwoNumbers(){
+  //   int a =93;
+  //   int b = 7;
+  //   int sum = a + b;
+  //   print( "The sum of $a and $b is $sum");
+  // }
+  // print("This is function 1");
+
+
+  // printMyDetails();
+  // print("This is  function 2");
+  // addTwoNumbers();
+
+  int calculateTotalMarls(){
+int num1 =32;
+int num2 = 45;
+int num3 = 78;
+int num4 = 90;
+int totalMarks = num1 + num2 + num3 + num4;
+
+    return totalMarks;
   }
 
-  addTwoNumbers(){
-    int a =93;
-    int b = 7;
-    int sum = a + b;
-    print( "The sum of $a and $b is $sum");
-  }
-  print("This is function 1");
+int totalMarks = calculateTotalMarls();
+print(totalMarks);
 
-
-  printMyDetails();
-  print("This is  function 2");
-  addTwoNumbers();
 }
