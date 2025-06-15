1c937d632da0a06ad0b52576adb07350accada3a
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-06-15 13:04:53 +0500

    default constructor

--- class.dart
+++ class.dart
@@ -1,57 +1,76 @@
-// class Person{
-//   String? pname;
-//   int? page ;
+// // class Person{
+// //   String? pname;
+// //   int? page ;
 
-//   action(){
-//     print("Hello, my name is $pname and I am studyin flutter.");
-//   }
-//   Person(name, age){
-// pname = name;
-// page=age;
+// //   action(){
+// //     print("Hello, my name is $pname and I am studyin flutter.");
+// //   }
+// //   Person(name, age){
+// // pname = name;
+// // page=age;
+// //   }
+
+// // }
+
+// // void main(){
+// //   Person p1 = new Person('hadi', 21);
+// //   p1.action();
+// //  print(p1.pname);
+// //   print(p1.page);
+// // }
+
+// class Device {
+//   String? name;
+//   String? brand;
+//   bool? isAndriod;
+//   bool? isIOS;
+
+// // Default constructor
+
+//   Device(name, brand) {
+
+//     // Use of this keyword
+//     this.name = name;
+//     this.brand = brand;
 //   }
 
+// // Named constructors
+//   Device.isAndriod() {
+//      isAndriod = true;
+//   }
+
+//   Device.isIOS() {
+//      isIOS = true;
+//   }
+
+//   action() {
+//     print("Hello, my device is $name and brand is $brand.");
+//     print("Is it Apple? ${isIOS}");
+//   }
 // }
 
-// void main(){
-//   Person p1 = new Person('hadi', 21);
-//   p1.action();
-//  print(p1.pname);
-//   print(p1.page);
+// void main() {
+//   Device d1 = new Device('iPhone 14', 'Apple');
+//   d1.action();
+//   Device d2 = new Device.isIOS();
+//   d2.action();
 // }
 
-class Device {
-  String? name;
-  String? brand;
-  bool? isAndriod;
-  bool? isIOS;
 
-// Default constructor
+class Myclass {
+  var a = 18;
+  String name ="";
 
-  Device(name, brand) {
 
-    // Use of this keyword
-    this.name = name;
-    this.brand = brand;
-  }
+Myclass (this.name, this.a);
 
-// Named constructors
-  Device.isAndriod() {
-     isAndriod = true;
-  }
 
-  Device.isIOS() {
-     isIOS = true;
-  }
-
-  action() {
-    print("Hello, my device is $name and brand is $brand.");
-    print("Is it Apple? ${isIOS}");
-  }
+Printa(){
+  print(a);
+}
 }
 
-void main() {
-  Device d1 = new Device('iPhone 14', 'Apple');
-  d1.action();
-  Device d2 = new Device.isIOS();
-  d2.action();
+void main(){
+Myclass a = Myclass("ahmed",12);
+a.Printa();
 }
